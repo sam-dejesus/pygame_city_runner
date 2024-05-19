1a0e0d5cd7db6f6b6600ad459f7f32229395f434
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-05-19 14:46:53 -0400

    added images, folders, and the index file. also set the screen size and gave it a name

---
+++ index.py
@@ -0,0 +1,11 @@
+# imports pygame lib
+import pygame
+from pygame.locals import *
+pygame.init()
+
+# screen size
+screen = pygame.display.set_mode((1280, 720))
+
+# title of the game when program starts
+pygame.display.set_caption('City Runner')
+
