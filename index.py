5790601d1b2adec342ef671cf02e6f4a12a61fd0
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-05-31 16:32:12 -0400

    added new animations for enemies and put them in a new file

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import random
+from classes import Enemy
 
 pygame.init()
 
@@ -125,38 +126,6 @@ class Player(pygame.sprite.Sprite):
                 self.vel_y = 0
                 self.on_ground = True
                 self.is_jumping = False
-
-    def draw_hitbox(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect, 2)
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        self.index = 0
-        self.counter = 0
-        for num in range(1, 9):
-            img = pygame.image.load(f'assets/img/player/run/Run-{num}.png')
-            self.images.append(img)
-        self.image = self.images[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.passed = False
-
-    def update(self, game_over):
-        if not game_over:
-            self.rect.x -= 5
-            if self.rect.right < 0:
-                self.kill()
-            self.counter += 1
-            cooldown = 5
-            if self.counter > cooldown:
-                self.counter = 0
-                self.index += 1
-                if self.index >= len(self.images):
-                    self.index = 0
-            self.image = self.images[self.index]
-            self.image = pygame.transform.flip(self.image, True, False)
 
     def draw_hitbox(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect, 2)
